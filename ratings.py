c589f991e0762dcce8cd90533acd914d47b622fe
“Julia <juliaabkhan@gmail.com>
2022-10-03 13:17:39 -0500

    Finished basic instructions. Prints alphabetized restaurants and their ratings.
    Co-Authored-by: chue chueyangib@gmail.com

--- ratings.py
+++ ratings.py
@@ -1,4 +1,13 @@
 """Restaurant rating lister."""
 
+filename = "scores.txt"
+restaurant_ratings = {}
 
-# put your code here
+for line in open(filename):
+    line = line.rstrip()
+    restaurant = line.split(":")[0]
+    rating = line.split(":")[1]
+    restaurant_ratings[restaurant] = rating
+
+for restaurant, rating in sorted(restaurant_ratings.items()):
+    print(f"{restaurant} is rated at {rating}.")
